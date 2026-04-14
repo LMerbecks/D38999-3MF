ea8a4008af5005fb28455baca13ee45413845f62
Leander Merbecks <leander.merbecks@gmx.de>
2026-04-14 14:05:50 +0200

    debugged insert generator. Works now for pin inserts. Socket Insert model needed.

--- scripts/insert_generator.py
+++ scripts/insert_generator.py
@@ -5,24 +5,34 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
+import pdb
 from dataclasses import dataclass
 
 SHELL_SIZES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J']
-SHELL_SIZES_CIVIL = [9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
+CIVIL_TO_MIL_SHELL_SIZES = {'9': 'A', '11': 'B', '13': 'C', '15': 'D',
+                             '17': 'E', '19': 'F', '21': 'G', '23': 'H', '25': 'J'}
 
 DOMAIN_3MF = '.3mf'
+DEFAULT_
 
 BOOLEAN_CUT_CODE = 1
 TOOL_BODY_ID = 'Body'
 PIN_INSERT_DOCUMENT_NAME = Path('../models/PinInserts.FCStd')
 if Path.exists(PIN_INSERT_DOCUMENT_NAME):
-    PIN_INSERT_DOCUMENT = FreeCAD.openDocument(PIN_INSERT_DOCUMENT_NAME)
+    PIN_INSERT_DOCUMENT = FreeCAD.openDocument(str(PIN_INSERT_DOCUMENT_NAME))
 
 SOCKET_INSERT_DOCUMENT_NAME = Path('../models/SocketInserts.FCStd')
 if Path.exists(SOCKET_INSERT_DOCUMENT_NAME):
     SOCKET_INSERT_DOCUMENT = FreeCAD.openDocument(SOCKET_INSERT_DOCUMENT_NAME)
 
-ARRANGEMENT_CONFIGURATIONS = ["9-3_contact_positions.csv"]
+USE_ALL_CONFIGS = False
+
+ARRANGEMENT_CONFIGURATIONS = ["9-3_contact_positions.csv", 
+                              "11-2_contact_positions.csv",
+                              "11-4_contact_positions.csv",
+                              "11-5_contact_positions.csv"]
+if USE_ALL_CONFIGS:
+    ARRANGEMENT_CONFIGURATIONS = []
 
 DUMMY_LABEL_TEMPLATE = "Size{shell_size}InsertDummy"
 
@@ -30,7 +40,7 @@ DUMMY_LABEL_TEMPLATE = "Size{shell_size}InsertDummy"
 class InsertFactory():
 
     gender: str
-    output_dir: Path=Path('../output/inserts/')
+    output_dir: Path=Path()
     input_dir: Path=Path('../dimensions')
 
     def __post_init__(self):
@@ -42,7 +52,8 @@ class InsertFactory():
             self.root_document = SOCKET_INSERT_DOCUMENT
 
     def generate_insert(self, arrangement_config: Path):
-        shell_size, contact_positions = self.parse_arrangment_csv(arrangement_config)
+        arrangement_csv = self.input_dir / arrangement_config
+        shell_size, contact_positions = self.parse_arrangement_csv(arrangement_csv)
         tool_body = self.get_contact_tool_body()
         insert_dummy = self.get_insert_dummy(shell_size)
         insert_identifier = self.extract_insert_name(arrangement_config)
@@ -57,19 +68,19 @@ class InsertFactory():
             self.translate_contact_tool(current_contact_tool, contact_x, contact_y)
             contact_tools.append(current_contact_tool)
         
-        insert_body = self.create_contact_cutouts(insert_body, contact_tools)
+        self.create_contact_cutouts(insert_body, contact_tools)
         self.export_insert(insert_body, insert_identifier)
 
     def parse_arrangement_csv(self,csv_filename:Path)->tuple[str, np.array]:
-        file_name = csv_filename.stem()
-        shell_size = file_name.split('-')
-        positions = self.get_contact_positions()
+        file_name = csv_filename.stem
+        shell_size = file_name.split('-')[0]
+        positions = self.get_contact_positions(csv_filename)
         if self.gender == 'S':
             positions = self.invert_positions(positions)
             return positions
-        return positions
+        return CIVIL_TO_MIL_SHELL_SIZES[shell_size], positions
     
-    def get_contact_positions(csv_filename:Path)->np.array:
+    def get_contact_positions(self, csv_filename:Path)->np.array:
         """Parse a csv containing the x and y positions of
         the contacts as noted when looking at the front face
         of the pin insert (!). Includes transform into the
@@ -88,7 +99,7 @@ class InsertFactory():
         if not ('x' in positions.columns and 'y' in positions.columns):
             raise ValueError("CSV needs to have x and y columns!")
 
-        positions = positions.loc[['y','x'],:].values
+        positions = positions.loc[:,['x','y']].values
         return positions
 
     def invert_positions(self,pin_contact_positions:np.array)->np.array:
@@ -103,17 +114,17 @@ class InsertFactory():
         """
         pass
 
-    def get_contact_tool_body(self)->FreeCAD.Object:
+    def get_contact_tool_body(self)->None:
         """Gets the contact tool body in the current
         document. The body identified by the string in
         TOOL_BODY_ID.
 
         Returns:
-            FreeCAD.Object: Body object of the contact tool
+            None: Body object of the contact tool
         """
         return self.root_document.getObject(TOOL_BODY_ID)
 
-    def get_insert_dummy(self,shell_size: str)->FreeCAD.Object:
+    def get_insert_dummy(self,shell_size: str)->None:
         """Gets the correct insert dummy (or template) body
         for a given shell size.
 
@@ -125,17 +136,17 @@ class InsertFactory():
             ValueError: If more or less than one dummy is found
 
         Returns:
-            FreeCAD.Object: The dummy insert body
+            None: The dummy insert body
         """
         if shell_size not in SHELL_SIZES:
             raise ValueError(f'{shell_size} is not a valid shell size: {SHELL_SIZES}')
-        dummy_label = DUMMY_LABEL_TEMPLATE.format(shell_size)
+        dummy_label = DUMMY_LABEL_TEMPLATE.format(shell_size=shell_size)
         dummy_body_candidates = self.root_document.getObjectsByLabel(dummy_label)
         if len(dummy_body_candidates) != 1:
             raise ValueError(f"{len(dummy_body_candidates)} Bodies found for {shell_size}!")
         return dummy_body_candidates[0]
 
-    def clone_body(self, original_body, identifier:str)->FreeCAD.Object:
+    def clone_body(self, original_body, identifier:str)->None:
         """Wraps the FreeCAD operations for cloning a body.
         
 
@@ -145,7 +156,7 @@ class InsertFactory():
             the new body
 
         Returns:
-            FreeCAD.Object: The cloned body
+            None: The cloned body
         """
         cloned_body = self.root_document.addObject('PartDesign::Body', identifier)
         clone_feature = self.root_document.addObject('PartDesign::FeatureBase', identifier + 'Clone')
@@ -168,18 +179,18 @@ class InsertFactory():
             translation 
             z (float): z position of the tool after translation
         """
-        position_vector = FreeCAD.Vector(x, 0, z)
+        position_vector = FreeCAD.Vector(0, x, z)
         euler_angles = FreeCAD.Rotation(0, 0, 0)
         rotation_axis = FreeCAD.Vector(0,0,0)
         new_placement = FreeCAD.Placement(position_vector, euler_angles, rotation_axis)
         tool_body.Placement = new_placement
 
-    def create_contact_cutouts(self,insert_base_body:FreeCAD.Object, contact_tools:list[FreeCAD.Object]):
+    def create_contact_cutouts(self,insert_base_body:None, contact_tools:list[None]):
         """Create the final insert body by cutting away all
         the contact bodies.
 
         Args:
-            insert_base_body (FreeCAD.Object): The base body
+            insert_base_body (None): The base body
             of the insert. This should be a clone of the template
             contact_tools (list): A list of the contact
             fixture tool bodies at the appropriate positions
@@ -201,17 +212,17 @@ class InsertFactory():
         Returns:
             str: insert name
         """
-        insert_name = arrangement_name.split('_')
+        insert_name = str(arrangement_name).split('_')[0]
         return insert_name
     
-    def export_insert(self, insert_body:FreeCAD.Object, insert_identifier:str):
+    def export_insert(self, insert_body, insert_identifier:str):
         """Recomputes the document and then exports the
         specified body to 3mf under the insert identifier
         name. The file is written to a subfolder specified
         by the gender of the insert. 
 
         Args:
-            insert_body (FreeCAD.Object): The body
+            insert_body (): The body
             containing the geometry to be exported
             insert_identifier (str): The identifier of the
             exported body
@@ -223,6 +234,7 @@ class InsertFactory():
         if not export_directory.exists():
             export_directory.mkdir()
         export_path = export_directory / (insert_identifier + DOMAIN_3MF)
+        export_path = str(export_path)
 
         if hasattr(Mesh, "exportOptions"):
             options = Mesh.exportOptions(export_path)
@@ -235,10 +247,10 @@ def main():
     pin_insert_factory = InsertFactory('P')
     # socket_insert_factory = InsertFactory('S')
     for arrangement in ARRANGEMENT_CONFIGURATIONS:
-        pin_insert_factory.generate_insert(arrangement)
+        pin_insert_factory.generate_insert(Path(arrangement))
         # socket_insert_factory.generate_insert(arrangement)
 
 
 
-if __name__ == '__main__':
+if __name__ == 'insert_generator':
     main()
